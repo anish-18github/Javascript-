839349b5b2ae7f3db491f1ebbfa5c49ad8aded49
anish-18github <108921271+anish-18github@users.noreply.github.com>
2024-06-16 15:26:50 +0000

    Number and math

---
+++ .github/Basics/number.js
@@ -0,0 +1,11 @@
+const Number = 123.9878
+console.log(Number.toPrecision(5))
+
+//                                         Math()
+console.log(Math.floor(Math.random() * 100) + 1)  //for randon number
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)  // for random number between 10 and 20  
+
